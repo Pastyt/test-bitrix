2fd505b399655268ab020b1a686673d664169a4e
Pavel Vladenkov <pavel.vladenkov.y@gmail.com>
2022-10-12 00:32:13 +0300

    add delete duplicates method

--- local/modules/currency.main/lib/CurrencyUpdate.php
+++ local/modules/currency.main/lib/CurrencyUpdate.php
@@ -13,6 +13,8 @@ class CurrencyUpdate
 
         foreach ($fetcher::fetchCurrency() as $currency) {
 
+            self::deleteDuplicates($currency['code'], $currency['date']);
+
             $new_currency = CurrencyTable::createObject();
             $new_currency->setCode($currency['code']);
             $new_currency->setDate($currency['date']);
@@ -21,5 +23,14 @@ class CurrencyUpdate
         }
 
         $collection->save();
+    }
+
+    private static function deleteDuplicates(string $code, string $date): void
+    {
+        $duplicates = CurrencyTable::getList(['filter' => ['code' => $code, 'date' => $date]])->fetchCollection();
+
+        foreach ($duplicates as $duplicate) {
+            $duplicate->delete();
+        }
     }
 }
