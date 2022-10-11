6a9ce9f64ec371a18b2e06f31bd1152de36b6e7a
Pavel Vladenkov <pavel.vladenkov.y@gmail.com>
2022-10-12 01:58:54 +0300

    add menu)

--- local/modules/currency.main/admin/menu.php
+++ local/modules/currency.main/admin/menu.php
@@ -3,7 +3,7 @@
 return [
     'parent_menu' => 'global_menu_services',
     'sort'        => 100,
-    'url'         => 'check_chart.php',
+    'url'         => 'perfmon_table.php?lang=ru&table_name=currency_main',
     'text'        => ':) Валюты',
     'title'       => 'Валюты',
     'module_id'   => 'currency.main',
